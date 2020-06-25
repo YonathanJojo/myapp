5d5f391568df92dbdcf00fc8e96a4503381b0520
YonathanJojo <jojoyonathan77@gmail.com>
2020-06-25 15:09:44 +0700

    Updates

---
+++ precache-manifest.4bf4b923daf904c00fcc0369a2149d36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90990c477dc665fa3bdbf45985c3e8e1",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "ae449a46a62d1fe75fb5",
+    "url": "/myapp/static/css/main.8c12dfdd.chunk.css"
+  },
+  {
+    "revision": "f8e0f7b4e1e7ab16e093",
+    "url": "/myapp/static/js/2.0fe82f9a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/myapp/static/js/2.0fe82f9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae449a46a62d1fe75fb5",
+    "url": "/myapp/static/js/main.6b926dd5.chunk.js"
+  },
+  {
+    "revision": "1716b5b1f163082744bb",
+    "url": "/myapp/static/js/runtime-main.aa379031.js"
+  }
+]);
